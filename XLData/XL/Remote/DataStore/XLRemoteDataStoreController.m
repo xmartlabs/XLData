103f74ef409957e535ef62f33a98b7c218b4888b
Mathias Claassen <mathias@xmartlabs.com>
2015-07-21 16:55:49 -0300

    Removing observers in viewWillDisappear

--- XLData/XL/Remote/DataStore/XLRemoteDataStoreController.m
+++ XLData/XL/Remote/DataStore/XLRemoteDataStoreController.m
@@ -170,7 +170,7 @@
 -(void)viewDidDisappear:(BOOL)animated
 {
     [super viewDidDisappear:animated];
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:AFNetworkingReachabilityDidChangeNotification object:nil];
 }
 
 -(void)refreshView:(UIRefreshControl *)refresh {
