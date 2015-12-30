86f99cbf8dd158bda3bb7c8855a9f138a7362bdb
Helen Olhausen <helen@xmartlabs.com>
2015-12-30 15:37:21 -0300

    fix for `attempt to delete and reload the same index path` on NSFetchedResultsChangeMove

--- XLData/XL/Local/CoreData/XLCoreDataController.m
+++ XLData/XL/Local/CoreData/XLCoreDataController.m
@@ -25,6 +25,8 @@
 
 #import "XLCoreDataController.h"
 
+#define SYSTEM_VERSION_LESS_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+
 @interface XLCoreDataController()
 
 @property (nonatomic) XLDataControllerType dataStoreControllerType;
@@ -232,7 +234,18 @@
                 [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:[self deleteRowAnimationForIndexPath:indexPath]];
                 break;
             case NSFetchedResultsChangeMove:
-                [self.tableView moveRowAtIndexPath:indexPath toIndexPath:newIndexPath];
+                // Fix for issue on NSFetchedResultsController: https://forums.developer.apple.com/thread/4999
+                // For an updated object, the didChangeObject: delegate method is called twice: Once with the NSFetchedResultsChangeUpdate event and then again with the NSFetchedResultsChangeMove event (and indexPath == newIndexPath). If indexPath with newIndexPath are equal, then the error occures.
+                // Beta fix, once Apple comes up with a fix this should be removed
+                if (SYSTEM_VERSION_LESS_THAN(@"9") && [indexPath isEqual:newIndexPath]) {
+                    break;
+                }
+                
+                [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
+                                      withRowAnimation:[self deleteRowAnimationForIndexPath:indexPath]];
+                [self.tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath]
+                                      withRowAnimation:[self insertRowAnimationForIndexPath:newIndexPath]];
+                
                 break;
             case NSFetchedResultsChangeUpdate:
                 [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:[self reloadRowAnimationForIndexPath:newIndexPath]];
